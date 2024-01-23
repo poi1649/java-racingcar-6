a1570eba9733f1688fc9431c16d31c6586d75894
leeshin20 <leeshin20@naver.com>
2024-01-24 00:37:50 +0900

    test: 시도할 회수가 0보다 작거나 자연수 이외의 값일 경우 예외처리 기능 테스트 코드 작성

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -23,4 +23,12 @@ public class InputTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5글자까지 입력 가능합니다.");
     }
+
+    @Test
+    void 시도할_회수_예외_처리_테스트(){
+        String number = "aab";
+        assertThatThrownBy(() -> Input.checkNumber(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 이상의 올바른 수를 입력해주세요.");
+    }
 }
