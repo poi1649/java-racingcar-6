9ec6a82b5ebc5330cc2fa84697c9a87340619d1a
leeshin20 <leeshin20@naver.com>
2024-01-23 23:45:33 +0900

    feat: 자동차 이름 예외처리 기능 추가

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -5,17 +5,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Input {
-    public void inputName(){
+    private static final int MAX_LENGTH_NAME = 5;
+
+    public static void inputName(){
         String names = Console.readLine();
         splitName(names);
     }
 
     public static ArrayList<String> splitName(String names){
-        return new ArrayList<>(Arrays.asList(names.split(",")));
+        ArrayList<String> nameList =  new ArrayList<>(Arrays.asList(names.split(",")));
+        checkName(nameList);
+        return nameList;
     }
 
     public static int inputNumber() {
         String number = Console.readLine();
         return Integer.parseInt(number);
+    }
+
+    public static void checkName(ArrayList<String> nameList) {
+        for(String name: nameList){
+            if(name.length() > MAX_LENGTH_NAME) {
+                throw new IllegalArgumentException("자동차 이름은 5글자까지 입력 가능합니다.");
+            }
+        }
     }
 }
