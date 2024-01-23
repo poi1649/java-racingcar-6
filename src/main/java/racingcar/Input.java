5e4b253b9933d3f23525d530f1dde81cbe724341
leeshin20 <leeshin20@naver.com>
2024-01-23 22:08:55 +0900

    feat: 자동차 이름을 콤마 기준으로 분할하는 기능 추가

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -1,9 +1,16 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Input {
     public void inputName(){
         String inputNames = Console.readLine();
+        splitName(inputNames);
+    }
+
+    public static ArrayList<String> splitName(String names){
+        return new ArrayList<>(Arrays.asList(names.split(",")));
     }
 }
