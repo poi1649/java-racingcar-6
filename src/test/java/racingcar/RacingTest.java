0ef9145c4231faff3f2c26d344cb19c39072391a
leeshin20 <leeshin20@naver.com>
2024-01-24 11:53:23 +0900

    test: 경주 진행시 자동차 이동 기능  테스트 코드 작성

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
@@ -12,5 +13,14 @@ public class RacingTest {
         cars.add(new Car("test"));
         Car firstCar = cars.get(0);
         assertThat(firstCar.getName()).isEqualTo("test");
+    }
+
+    @Test
+    void 경주_진행시_자동차_이동_테스트(){
+        ArrayList<Car> cars = new ArrayList<>();
+        cars.add(new Car("test"));
+        Car firstCar = cars.get(0);
+        firstCar.forward();
+        assertThat(firstCar.getRaceScore()).isEqualTo(1);
     }
 }
