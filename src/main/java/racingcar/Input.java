7869b28426f41fce7824afa790d24d78c666c029
leeshin20 <leeshin20@naver.com>
2024-02-01 11:32:49 +0900

    refactor: 숫자 입력 예외처리 세분화

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Input {
+    private static final int MAX_NAME_LENGTH = 5;
     private Input() {
 
     }
@@ -22,7 +23,7 @@ public class Input {
 
     public static void checkName(ArrayList<String> nameList) {
         for (String name : nameList) {
-            if (name.length() > 5) {
+            if (name.length() > MAX_NAME_LENGTH) {
                 throw new IllegalArgumentException("자동차 이름은 5글자까지 입력 가능합니다.");
             }
         }
@@ -36,8 +37,12 @@ public class Input {
 
     public static void checkNumber(String number) {
         if (!isNumber(number) || Integer.parseInt(number) <= 0) {
-            throw new IllegalArgumentException("1 이상의 올바른 수를 입력해주세요.");
+            if (Integer.parseInt(number) <= 0) {
+                throw new IllegalArgumentException("1 이상의 수를 입력해 주세요.");
+            }
+            throw new IllegalArgumentException("숫자를 입력해 주세요.");
         }
+
     }
 
     public static boolean isNumber(String number) {
