0547beb83d326a797da5b4e67140ebd04c1f286f
leeshin20 <leeshin20@naver.com>
2024-02-07 13:59:06 +0900

    style: 개행 추가 및 사용하지 않는 라이브러리 제거

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,12 +2,11 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 public class CarTest {
     private static final int FORWARD_THRESHOLD = 4;
+
     @Test
     void carMoveTest() {
         Car testCar = new Car("test");
