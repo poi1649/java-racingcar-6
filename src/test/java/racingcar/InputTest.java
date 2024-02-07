e1654154eb966adc6c9255f7d8489f8b6146e5f4
leeshin20 <leeshin20@naver.com>
2024-02-07 13:44:29 +0900

    test: ArrayList 타입 변경에 따른 테스트 코드 수정

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -4,13 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
+import java.util.List;
 
 public class InputTest {
     @Test
     void 자동차_이름_분할_테스트() {
         String names = "ura,zara";
-        ArrayList<String> result = Input.splitName(names);
+        List<String> result = Input.splitName(names);
         assertThat(result).contains("zara", "ura");
         assertThat(result).containsExactly("ura", "zara");
     }
